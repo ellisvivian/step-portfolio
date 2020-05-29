2c4cc8bb245ef7fdfe57ceedd11db4ff33fb6129
Vivian Ellis <vivian.c.ellis@gmail.com>
2020-05-29 12:28:07 -0700

    Address requested changes and avoid direct repeat of fun facts.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,108 +12,81 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+let fact_num_old = -1;
+
 /**
  * Adds a fun fact about me to the page.
  */
-function addFunFact() {
-  const fun_facts =
-      ['My favorite color is purple.', 'I have a little brother.', 'I am half-Chinese.', 'I am 18 years old.', 'My elbows are double-jointed.',
-      'I love Shrek (unironically).', 'I don\'t like eating many fruits.', 'I\'m allergic to cats.'];
+function addFunFact() { 
+  const fun_facts = [
+    'My favorite color is purple.', 
+    'I have a little brother.', 
+    'I am half-Chinese.', 
+    'I am 18 years old.', 
+    'My elbows are double-jointed.',
+    'I love Shrek (unironically).', 
+    'I don\'t like eating many fruits.', 
+    'I\'m allergic to cats.'
+  ];
 
-  // Pick a random fun fact.
-  const fun_fact = fun_facts[Math.floor(Math.random() * fun_facts.length)];
+  // Pick a random fun fact different from the current one.
+  let fact_num_new = Math.floor(Math.random() * fun_facts.length);
+  if (fact_num_old != -1) {
+    while (fact_num_old == fact_num_new) {
+      fact_num_new = Math.floor(Math.random() * fun_facts.length);
+    }
+  }
+  fact_num_old = fact_num_new;
 
   // Add it to the page.
   const factContainer = document.getElementById('fact-container');
-  factContainer.innerText = fun_fact;
+  factContainer.innerText = fun_facts[fact_num_new];
 }
 
 let count;
 let timer;
 
 /*
- * Displays the intital image and description once the page is loaded.
+ * Displays the initial image and description once the page is loaded.
  */
 function initialDisplay() {
-    count = -1;
-    start();
+  count = -1;
+  start();
 }
 
 /*
  * Changes the image and description displayed on the page.
  */
 function autoSlideshow() {
-    let experiences = document.getElementsByClassName('experience');
-    console.log(experiences);
-    console.log(experiences.length);
+  let experiences = document.getElementsByClassName('experience');
 
-    // Hide all images.
-    for (i = 0; i < experiences.length; i ++) {
-      experiences[i].style.display = "none";
-    }
+  // Hide all images and descriptions.
+  for (i = 0; i < experiences.length; i ++) {
+    experiences[i].style.display = "none";
+  }
     
-    // Update count and display next image. 
-    if (count == 3) {
-      count = 0;
-    } else {
-      count ++;
-    }
-    console.log(count);
-    experiences[count].style.display = "flex";
+  // Update count and display next image and description. 
+  if (count == 3) {
+    count = 0;
+  } else {
+    count ++;
+  }
+  experiences[count].style.display = "flex";
 }
 
 /*
  * Begins the slideshow.
  */
 function start() {
-    autoSlideshow();
-    timer = setInterval('autoSlideshow();', 6000)
-    document.getElementById('continue').style.display = "none";
+  autoSlideshow();
+  timer = setInterval('autoSlideshow();', 6000)
+  document.getElementById('continue').style.display = "none";
 }
 
 /*
  * Stops the slideshow.
  */
 function stop() {
-    clearInterval(timer); // Stops the automatic changing of the image and description.
-    document.getElementById('continue').style.display = "block";
+  clearInterval(timer); // Stops the automatic changing of the image and description.
+  document.getElementById('continue').style.display = "block";
 }
-
-// let image_sources = ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg', '/images/4.jpg']
-
-// let descriptions = ['Family has always been something really important to me, especially my cousins. ' + 
-//                     'Every year, we try to get together in what we call \'Cuz Unite\'. During Cuz Unites, ' +
-//                     'we go on wild adventures, play games, and just spend time catching up and enjoying ' +
-//                     'each other\'s company. Around my cousins I always feel loved and free to be myself.',
-    
-//                     'At Rice, students are randomly split into 11 residential colleges, which will serve as an ' +
-//                     'inclusive community throughout their four years on campus (and beyond). My college is ' +
-//                     'Wiess college, and from the moment I was welcomed during orientation week it has felt ' + 
-//                     'like home. From the friendly faces I see in commons every day to our quirky and exciting ' +
-//                     'traditions, I am truly grateful for my Team Family Wiess!', 
-
-//                     'The summer after my senior year of high school, I went on an epic month-long European vacation ' +
-//                     'with my family, where we visited The Netherlands, Belgium, France, Scotland, and England. It was ' +
-//                     'an amazing experience to get to see and explore so many different places, and I am so ' + 
-//                     'appreciative of my family for making it happen. Hopefully I can continue to travel the world ' +
-//                     'on my own someday.',
-                    
-//                     'Ever since I was really little and just twirling around the house with scarves, ' + 
-//                     'I\'ve always had a passion for dance. Whether it was performing on stage with my ' +
-//                     'competitive team, at a rally with my high school class, or in the quad with my kpop ' +
-//                     'dance club, I had so much fun expressing myself through movement. Sometimes the ' +
-//                     'competitive nature of the dance world got to me, but I would turn back to low-pressure ' + 
-//                     'dance environments with friends to remind me of the happiness it brought me.'];
-
-/*
- * Changes the image and description on the page.
- */ 
-// function changeDisplay() {
-//     if (count == 3) {
-//         count = 0;
-//     } else {
-//         count ++;
-//     }
-//     document.getElementById('img').src = image_sources[count];
-//     document.getElementById('description').innerText = descriptions[count];
-// }
